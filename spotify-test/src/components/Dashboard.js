932fddfd2b36b7ba3735f6215dc0239fd941fd81
Brian <brian.kitano@yale.edu>
2017-12-27 09:05:19 +0000

    basic functionality of personalized API requests

--- spotify-test/src/components/Dashboard.js
+++ spotify-test/src/components/Dashboard.js
@@ -2,14 +2,15 @@ import React, {Component} from 'react';
 import queryString from 'query-string';
 import spotifyWebAPI from 'spotify-web-api-node';
 
-import {Card, CardText, CardHeader} from 'material-ui/Card';
+import {Card, CardHeader, CardTitle} from 'material-ui/Card';
 
 class Dashboard extends Component {
     
     constructor(props) {
         super(props);
         this.state = {
-            name: ''
+            name: '',
+            tracks: []
         };
     }
     
@@ -20,25 +21,43 @@ class Dashboard extends Component {
         
         if(tokens !== null && localStorage.state === tokens.state) {
             localStorage.setItem('token', tokens.access_token);
-        }
         
-        // Working with the spotify api
-        
-        var Spotify = new spotifyWebAPI();
-        
-        Spotify.setAccessToken(tokens.access_token);
-        
-        // Get the authenticated user
-        Spotify.getMe().then( data => {
+            // Working with the spotify api
             
-            this.setState({name: data.body.display_name});
-            console.log('Some information about the authenticated user', data.body);
+            var Spotify = new spotifyWebAPI();
             
+            Spotify.setAccessToken(tokens.access_token);
             
-        }, function(err) {
-            console.log('Something went wrong!', err);
-        });
+            // Get the authenticated user
+            Spotify.getMe().then( data => {
+                
+                this.setState({name: data.body.display_name});
+                
+            }, function(err) {
+                console.log('Something went wrong!', err);
+            });
+            
+            Spotify.getMyTopTracks({limit: 10, time_range: 'long_term'}).then( data => {
 
+                data.body.items.map( item => {
+                    
+                    var track = {
+                        name: item.name,
+                        album: item.album.name,
+                        artist: item.artists[0].name,
+                        album_artwork: item.album.images[1].url
+                    };
+                    
+                    this.state.tracks.push(track);
+
+                    return null;
+                });
+                
+            }, function(err) {
+                console.log(err);
+            })
+            
+        }
     }
     
     render() {
@@ -46,12 +65,17 @@ class Dashboard extends Component {
         return (
             <div>
                 <Card>
-                    <CardHeader>
-                        Info
-                    </CardHeader>
-                    <CardText>
-                        {this.state.name}
-                    </CardText>
+                <CardTitle title={this.state.name.split(" ")[0] + "\'s top tracks"} />
+                {this.state.tracks.map( track => {
+                    return (
+                        <Card>
+                            <CardHeader 
+                                title={track.name}
+                                subtitle={track.artist}
+                                avatar={track.album_artwork} />
+                        </Card>
+                    );
+                })}
                 </Card>
             </div>
             );
