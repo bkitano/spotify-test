4c9cfa4cd9f3c61b49bb698e5ab84de16826e65f
Brian <brian.kitano@yale.edu>
2017-12-27 08:11:43 +0000

    pulling clean data from Spotify

--- spotify-test/src/components/Dashboard.js
+++ spotify-test/src/components/Dashboard.js
@@ -1,17 +1,55 @@
 import React, {Component} from 'react';
 import queryString from 'query-string';
+import spotifyWebAPI from 'spotify-web-api-node';
+
+import {Card, CardText, CardHeader} from 'material-ui/Card';
 
 class Dashboard extends Component {
     
-    render() {
+    constructor(props) {
+        super(props);
+        this.state = {
+            a: 1
+        };
+    }
+    
+    componentDidMount() {
         
         const tokenString = window.location.href.split('/')[4];
         var tokens = queryString.parse(tokenString);
+        
         console.log(tokens);
+        
+        if(tokens !== null && localStorage.state == tokens.state) {
+            localStorage.setItem('token', tokens.access_token);
+        }
+        
+        // Working with the spotify api
+        
+        var Spotify = new spotifyWebAPI();
+        
+        Spotify.setAccessToken(tokens.access_token);
+        
+        Spotify.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE')
+            .then(function(data) {
+            console.log('Artist albums', data.body);
+            }, function(err) {
+            console.error(err);
+            });
+    }
+    
+    render() {
         
         return (
             <div>
-                Logged In
+                <Card>
+                    <CardHeader>
+                        Token
+                    </CardHeader>
+                    <CardText>
+                        {localStorage.getItem('token')}
+                    </CardText>
+                </Card>
             </div>
             );
     }
